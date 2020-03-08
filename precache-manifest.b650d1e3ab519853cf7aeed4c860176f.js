afcf5ef131ccadccd1e4e2a7d66b576df6af3aaa
Jochen Diekenbrock <1625222+JochenDiekenbrock@users.noreply.github.com>
2020-03-09 00:10:44 +0100

    Updates

---
+++ precache-manifest.b650d1e3ab519853cf7aeed4c860176f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b75d88dd900b7f307c457f0ce6bdd8d",
+    "url": "/wuerfelschlange/index.html"
+  },
+  {
+    "revision": "6823f0c39dfcabc11098",
+    "url": "/wuerfelschlange/static/css/main.6d06e566.chunk.css"
+  },
+  {
+    "revision": "5af318fdf870f2d7afda",
+    "url": "/wuerfelschlange/static/js/2.afbc1fca.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/wuerfelschlange/static/js/2.afbc1fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6823f0c39dfcabc11098",
+    "url": "/wuerfelschlange/static/js/main.d05fa66e.chunk.js"
+  },
+  {
+    "revision": "99772d06d75c596930d6",
+    "url": "/wuerfelschlange/static/js/runtime-main.66713cc9.js"
+  }
+]);
